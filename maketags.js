7567bc9b46b50ef8a3124b0d86783e81802bd247
Trey Jackson <jacksrf@gmail.com>
2014-11-25 12:25:14 -0500

    finished

---
+++ maketags.js
@@ -0,0 +1,6 @@
+var tags = process.argv[2];
+var word = process.argv[3];
+
+//if tags are in the array then move on, if not "this tag does not work";
+
+console.log("<"+ tags + ">" + word + "<" + tags + "/>");
