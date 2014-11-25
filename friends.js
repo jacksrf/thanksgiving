911cfc5c3161f464115e0d7ea2859ca8d5bf7760
Trey Jackson <jacksrf@gmail.com>
2014-11-25 11:49:53 -0500

    friends

---
+++ friends.js
@@ -0,0 +1,5 @@
+var friendsCast = ["Jennifer Aniston","Courteney Cox","Lisa Kudrow","Matt LeBlanc","Matthew Perry","David Schwimmer"];
+
+var randomFriend = friendsCast[Math.floor(Math.random()*(friendsCast.length-1)+1)];
+
+console.log(randomFriend);
